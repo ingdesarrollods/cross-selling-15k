63609f5ffd583bf98b695c1dab1fb6bffdbc486e
Johan Cortes <jcortes@dayscript.com>
2019-07-25 12:36:18 -0500

    Modificaciones de login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $result = (new EmailController)->contacto();
+        //$result = (new EmailController)->contacto();
         return view('home');
     }
 }
